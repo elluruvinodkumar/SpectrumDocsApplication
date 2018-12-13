5f51203d16759ee12e46cbebae9e7f8642b2008b
ramanareddy <ramanareddy0012@gmail.com>
2018-12-13 16:38:36 +0530

    Patients list including personal information

--- models/doctor.js
+++ models/doctor.js
@@ -1,15 +1,29 @@
 let Doctordb = require('../app/doctor');
+let Personaldb = require('../app/personalinfo');
 
 let Doctor = {
 
     findByEmail : (Doctor,callback) => {
-            Doctordb.find({ email:Doctor.email},{username:1,_id:0,__v:0 }).exec()
+            Doctordb.find({ email:Doctor.email},{username:1,_id:0}).exec()
             .then((result) => {
-                callback(result);
+            
+                var promises = result.map(function(obj){
+                    console.log('promises calling');
+                    let patients;
+                    return Personaldb.findOne({username:obj.username}).then(function(results){
+                       patients = results;
+                       return patients;
+                    })
+                  })
+                  
+                  Promise.all(promises).then(function(results) {
+                    callback({response:3, patientsList:results});
+                })
             })
+        
             .catch((error) => {
                 callback(error);
-            }
+            })
     }
 
 };
