6fdc9ea097dd6790da3411c9f35d3ebce39b8930
ramanareddy <ramanareddy0012@gmail.com>
2018-12-13 11:10:01 +0530

    list of patients fetching

--- models/doctor.js
+++ models/doctor.js
@@ -3,7 +3,7 @@ let Doctordb = require('../app/doctor');
 let Doctor = {
 
     findByEmail : (Doctor,callback) => {
-            Doctordb.find({ email:Doctor.email},{username:1,_id:0,_v:0 }).exec()
+            Doctordb.find({ email:Doctor.email},{username:1,_id:0,__v:0 }).exec()
             .then((result) => {
                 callback(result);
             })
