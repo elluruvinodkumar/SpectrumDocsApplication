47f514878871bf80b49112a2a4ec40bb51ca24a6
ramanareddy <ramanareddy0012@gmail.com>
2018-12-17 09:27:19 +0530

    changed error handling code

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ app.use('/spectrodoctor', doctorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
